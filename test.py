2734e5f8d9d7cb029efae9d7390b49faace8a49f
BasementsAreCosy <94196763+BasementsAreCosy@users.noreply.github.com>
2023-12-15 18:24:54 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import physics as ph
 import time
 
-myEnv = ph.simEnvironment([ph.Particle(5, (0, 0), vectors=[ph.Vector((1, 1))])], ph.Vector((0, -9.8)))
-for i in range(10):
+myEnv = ph.simEnvironment([ph.Object(5, (0, 0), (0, 0), vectors=[ph.Vector((1, 1))])], [ph.Vector((0, -9.8))])
+for i in range(30):
+    print(f'------------------\n{i}\n')
     print(myEnv)
     myEnv.timeStep(1)
     time.sleep(1)
